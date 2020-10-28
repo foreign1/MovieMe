af6b339bfa422a165b7e429f2731f054fa62cacf
unknown <Jenyojohn24@gmail.com>
2020-10-28 23:10:45 +1345

    setup and style MovieMe

---
+++ app.js
@@ -0,0 +1,29 @@
+// Imports
+const express = require("express");
+const app = express();
+const port = 5000;
+
+// Static Files
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
+
+// Set View's
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+// Navigation
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/all.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(
+    __dirname + "/views/about.html",
+    __dirname + "/public/css/styles.css",
+    __dirname + "/public/js/main.js"
+  );
+});
+
+app.listen(port, () => console.info(`App listening on port ${port}`));
